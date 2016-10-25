91bcdb15ea83deba2e491808fd78b7bbd9b8b22e
Gilles Fourestey <gilles.fourestey@epfl.ch>
2016-10-25 16:09:58 +0200

    big test comparison

--- GradientBenchmark/main.cpp
+++ GradientBenchmark/main.cpp
@@ -13,13 +13,14 @@
 #include <fstream>
 //
 #include "structure.h"
+#include "timer.h"
 //
 /** for both gradient and second derivatives **/
 static struct point rotateCoordinateSystem(struct point P, double theta);
 
 /** gradient **/
 struct point module_potentialDerivatives_totalGradient(const runmode_param *runmode, const struct point *pImage, const struct Potential *lens);
-struct point potentialDerivatives_totalGradient(const runmode_param *runmode, const struct point *pImage, const struct Potential *lens);
+struct point potentialDerivatives_totalGradient(const runmode_param *runmode, const struct point *pImage, const struct Potential *lens, int nhalos);
 //
 static struct point grad_halo(const struct point *pImage, const struct Potential *lens);
 
@@ -38,7 +39,6 @@ int main()
 	//
 	//Variable creation
 	//
-	struct timeval t1, t2, t3, t4;
 	runmode_param runmodesmall;
 	runmode_param runmodemedium;
 	runmode_param runmodebig;
@@ -60,39 +60,42 @@ int main()
 	{
 		ilens = &lens[i];
 		//
-		ilens->position.x = ilens->position.y = 0.;
-		ilens->type = 8;
+		ilens->vdisp       = 1.;
+		ilens->position.x  = ilens->position.y = 0.;
+		ilens->type        = 8;
 		ilens->ellipticity = 0.11;
 		ilens->ellipticity_potential = 0.;
 		ilens->ellipticity_angle = 0.;
-		ilens->rcut = 5.;
-		ilens->rcore = 1;
-		ilens->weight = 0;
-		ilens->rscale = 0;
-		ilens->exponent = 0;
-		ilens->alpha = 0.;
+		ilens->rcut 	   = 5.;
+		ilens->rcore       = 1;
+		ilens->weight      = 0;
+		ilens->rscale      = 0;
+		ilens->exponent    = 0;
+		ilens->alpha       = 0.;
 		ilens->einasto_kappacritic = 0;
-		ilens->z = 0.4;
+		ilens->z           = 0.4;
 		module_readParameters_calculatePotentialparameter(ilens);
 	}
 	//
-	//gettimeofday(&t1, 0);
-	module_potentialDerivatives_totalGradient(&runmodesmall,&image, lens);
-	//gettimeofday(&t2, 0);
-	module_potentialDerivatives_totalGradient(&runmodemedium,&image, lens);
-	gettimeofday(&t3, 0);
-	point grad = module_potentialDerivatives_totalGradient(&runmodebig, &image, lens);
-	gettimeofday(&t4, 0);
-	//
-	double time1 = (1000000.0*(t2.tv_sec-t1.tv_sec) + t2.tv_usec-t1.tv_usec)/1000000.0;
-	double time2 = (1000000.0*(t3.tv_sec-t2.tv_sec) + t3.tv_usec-t2.tv_usec)/1000000.0;
-	double time3 = (1000000.0*(t4.tv_sec-t3.tv_sec) + t4.tv_usec-t3.tv_usec)/1000000.0;
-
 	std::cout << "Benchmark for Gradient Calculation "<< std::endl;
-	//std::cout << "Sample size " << small << ": " << time1 << std::endl;
-	//std::cout << "Sample size " << medium << ": " << time2 << std::endl;
-	std::cout << "Sample size " << big << ": " << time3 << "s., point = " << grad.x << " " << grad.y << std::endl;
-
+	point grad; // store the result
+	//
+	//gettimeofday(&t1, 0);
+	//
+	//module_potentialDerivatives_totalGradient(&runmodesmall,&image, lens);
+	double t1;
+	//
+	t1 = -myseconds();
+	grad = module_potentialDerivatives_totalGradient(&runmodebig, &image, lens);
+	t1 += myseconds();
+	std::cout << "Sample size " << big << ": " << t1 << "s., point = " << grad.x << " " << grad.y << std::endl;
+	//
+	t1 = -myseconds();
+	grad = potentialDerivatives_totalGradient(&runmodebig, &image, lens, big);
+	t1 += myseconds();
+	std::cout << "Sample size " << big << ": " << t1 << "s., point = " << grad.x << " " << grad.y << std::endl;
+	//
+#if 0
 	std::ofstream myfile;
 	myfile.open ("BenchmarkGrad.txt");
 	myfile << "Benchmark for Gradient Calculation "<< std::endl;
@@ -100,6 +103,6 @@ int main()
 	myfile << "Sample size " << medium << ": " << time2 << std::endl;
 	myfile << "Sample size " << big << ": " << time3 << std::endl;
 	myfile.close();
-
+#endif
 }
 
