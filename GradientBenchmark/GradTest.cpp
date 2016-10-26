4cf7755ae88584f4fcf4be57f39d533ba0d83c32
cerschae <christophernstrerne.schaefer@epfl.ch>
2016-10-26 14:16:47 +0200

    Divided big test into three smaller ones

--- GradientBenchmark/GradTest.cpp
+++ GradientBenchmark/GradTest.cpp
@@ -43,63 +43,99 @@ complex piemd_1derivatives(double x, double y, double eps, double rc)
 
 void gradtest()
 {
-	/** Testing for Gradient function **/
 
 	//////Setting the problem
 	int big(1);
 	runmode_param runmode;
-	runmode.nhalos = big;
 	point image;
+
+	runmode.nhalos = big;
 	image.x = image.y = 2;
 
-	///////theoretical gradient calculation
+	/////// Test for 5 PIEMD mass distribution:
+	//1: module_readParameters_calculatePotentialparameter
+	//2: piemd_1derivatives_ci05
+	//3: gradient
+
+
+	//1: module_readParameters_calculatePotentialparameter
+
+	///////theoretical b0, ellipticity calculation
+
 	//Init
-	point gradtheo;
-	gradtheo.x = gradtheo.y = 0;
-	double b0(0), vdisp(1.), ellipticity_potential(0), ellipticity(0.11);
-	complex zis;
+	double b0(0),ellipticity_potential(0);
+	double vdisp(1.),ellipticity(0.11);
+
 	//Computation
 	ellipticity_potential = (1. - sqrt(1 - ellipticity * ellipticity)) / ellipticity;
 	b0 = 6.*pi_c2 * vdisp * vdisp;
 
+	///////numerical b0, ellipticity_potential calculation
 
-	//Doing something....
-    zis = piemd_1derivatives(image.x, image.y, ellipticity_potential, 1);
+	//Init
+		Potential  *ilens;
+		Potential lens[big];
 
-    gradtheo.x=b0* zis.re;
-    gradtheo.y=b0 * zis.im;
+		for (int i = 0; i <big; ++i){
+			ilens = &lens[i];
 
+		    ilens->position.x = ilens->position.y = 0.;
+		    ilens->type = 8;
+		    ilens->ellipticity = 0.11;
+		    ilens->ellipticity_potential = 0.;
+		    ilens->ellipticity_angle = 0.;
+		    ilens->vdisp = 1.;
+		    ilens->rcut = 5.;
+		    ilens->rcore = 1;
+		    ilens->weight = 0;
+		    ilens->rscale = 0;
+		    ilens->exponent = 0;
+		    ilens->alpha = 0.;
+		    ilens->einasto_kappacritic = 0;
+		    ilens->z = 0.4;
+		    //Computation
+		    module_readParameters_calculatePotentialparameter(ilens);
 
+		}
 
-	///////Gradient calculation using functions
-
-    //Init
-	point grad;
-	Potential  *ilens;
-	Potential lens[big];
-
-	grad.x = grad.y = 0;
-	for (int i = 0; i <big; ++i){
-		ilens = &lens[i];
-
-	    ilens->position.x = ilens->position.y = 0.;
-	    ilens->type = 8;
-	    ilens->ellipticity = 0.11;
-	    ilens->ellipticity_potential = 0.;
-	    ilens->ellipticity_angle = 0.;
-	    ilens->vdisp = 1.;
-	    ilens->rcut = 5.;
-	    ilens->rcore = 1;
-	    ilens->weight = 0;
-	    ilens->rscale = 0;
-	    ilens->exponent = 0;
-	    ilens->alpha = 0.;
-	    ilens->einasto_kappacritic = 0;
-	    ilens->z = 0.4;
-	    module_readParameters_calculatePotentialparameter(ilens);
-
+	if(b0 != lens[0].b0){
+		std::cerr << " b0: Theoretically we should have "<< b0 << "  and  " << lens[0].b0 << std::endl;
+		exit(0);
 	}
-	//Init SoA
+	if(ellipticity_potential != lens[0].ellipticity_potential){
+		std::cerr << " ellipticity_potential: Theoretically we should have "<< ellipticity_potential << "  and  " << lens[0].ellipticity_potential << std::endl;
+		exit(0);
+	}
+
+	//2: piemd_1derivatives_ci05
+
+	//Init
+	complex ztheo, zcomput;
+	///////theoretical piemd
+	ztheo = piemd_1derivatives(image.x, image.y, ellipticity_potential, 1);
+	///////numerical piemd
+	zcomput =  piemd_1derivatives_ci05(image.x, image.y, ellipticity_potential, 1);
+
+	if(ztheo.re != zcomput.re){
+		std::cerr << " z_piemd.re: Theoretically we should have "<< ztheo.re << "  and  " << zcomput.re << std::endl;
+		exit(0);
+	}
+	if(ztheo.im != zcomput.im){
+		std::cerr << " z_piemd.im: Theoretically we should have "<< ztheo.im << "  and  " << zcomput.im << std::endl;
+		exit(0);
+	}
+
+	//3: gradient
+	///////theoretical
+	//Init
+	point gradtheo,grad;
+	grad.x = grad.y = 0;
+	//Computation
+    gradtheo.x=b0* ztheo.re;
+    gradtheo.y=b0 * ztheo.im;
+
+    ///////Computation
+    //Init SoA
 	PotentialSet lenses;
 	lenses.type = 	new int[big];
 	lenses.x  = 	new double[big];
@@ -128,11 +164,15 @@ void gradtest()
 	// Computation
 	grad = module_potentialDerivatives_totalGradient(&runmode,&image, &lenses);
 
+	if(gradtheo.x != grad.x){
+		std::cerr << " grad.x: Theoretically we should have "<< gradtheo.x << "  and  " << grad.x << std::endl;
+		exit(0);
+	}
+	if(gradtheo.y != grad.y){
+		std::cerr << " grad.y: Theoretically we should have "<< gradtheo.y << "  and  " << grad.y << std::endl;
+		exit(0);
+	}
 
-	////////Checking
-
-	std::cout << "Theoretically we should have "<< gradtheo.x << "  and  " << gradtheo.y << std::endl;
-	std::cout << "and the function returns "<< grad.x << "  and  " << grad.y << std::endl;
-
+	std::cout << "Everythings fine, No test failed " << std::endl;
 }
 
