65f98118f1316eddf68be648538dadd462281026
Gilles Fourestey <gilles.fourestey@epfl.ch>
2016-12-25 21:46:23 +0100

    useful functions

---
+++ src/utils.hpp
@@ -0,0 +1,29 @@
+// utils.hpp is part of lenstoolhpc
+//
+#ifndef __UTILS__
+#define __UTILS__
+
+void quicksort(int *A, int len)
+{
+	if (len < 2) return;
+	//
+	int pivot = A[len/2];
+	//
+	int i, j;
+	for (i = 0, j = len - 1; ; i++, j--)
+	{
+		while (A[i] < pivot) i++;
+		while (A[j] > pivot) j--;
+
+		if (i >= j) break;
+
+		int temp = A[i];
+		A[i]     = A[j];
+		A[j]     = temp;
+	}
+	//
+	quicksort(A, i);
+	quicksort(A + i, len - i);
+}
+
+#endif
