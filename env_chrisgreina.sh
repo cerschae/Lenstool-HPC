d650e40970c73c730a2817aa9e2eecb6a7230b3e
Christoph Schaefer <cerschae@greina17.cm.cluster>
2017-02-23 15:44:39 +0100

    updated Makefile with env variables
    Former-commit-id: f1e9d2705e479871ca2b1f394381254bb2e4f83d

---
+++ env_chrisgreina.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export LENSTOOL_ROOT=/users/cerschae/Libs/lenstool-6.8.1
+export LENSTOOLHPC_ROOT=/users/cerschae/lenstool-hpc
+export CFITSIO_ROOT=/users/cerschae/Libs/cfitsio
+export WCSTOOL_ROOT=/users/cerschae/Libs/wcstools-3.9.4/libwcs
+export GSL_ROOT=/users/cerschae/Libs/gsl-2.3
+export LD_LIBRARY_PATH+=:/users/cerschae/lenstool-hpc/src
+export LD_LIBRARY_PATH+=:/users/cerschae/Libs/gsl-2.3/lib
