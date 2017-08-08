6a0f69137dd75aab9e0baac19a700847c8279c7e
Gilles Fourestey <gilles.fourestey@epfl.ch>
2017-08-08 14:54:55 +0200

    update after the rebase
    Former-commit-id: 306c9b5d4bb3cdca30b00a79380f1b03d500c510

--- env_gilles.sh
+++ env_gilles.sh
@@ -1,5 +1,5 @@
 export LENSTOOL_ROOT=/users/fgilles/Projects/lenstool-6.8.1/
-export LENSTOOLHPC_ROOT=/users/fgilles/GPU-Projects/lenstool-hpc-christoph/
+export LENSTOOLHPC_ROOT=/users/fgilles/GPU-Projects/lenstool-hpc-master/
 export CFITSIO_ROOT=/users/fgilles/Projects/Libs/cfitsio/
 export WCSTOOL_ROOT=/users/fgilles/Projects/Libs/wcstools-3.9.4/libwcs/
 export GSL_ROOT=/users/fgilles/Projects/Libs/gsl-2.2/
