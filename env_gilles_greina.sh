dcac6b943a2ae1778c53569d6e7b5eedf58598b9
gilles fourestey <gilles.fourestey@epfl.ch>
2018-10-31 21:02:14 +0100

    working version of the delensing on the GPU
    Former-commit-id: d063401aa7202ce4bb9abeb7518d4d5876f3a2d0

---
+++ env_gilles_greina.sh
@@ -0,0 +1,25 @@
+#echo "\$BASH_SOURCE ${BASH_SOURCE[@]}"
+#echo "$PWD/${BASH_SOURCE[@]}"
+#echo dirname "$PWD/${BASH_SOURCE[@]}"
+#echo $(realpath "$(dirname "$PWD/${BASH_SOURCE[@]}")")
+MYPATH=$(realpath "$(dirname "$PWD/${BASH_SOURCE[@]}")")
+#echo $MYPATH
+export LENSTOOL_ROOT=/scratch/fgilles/Projects/lenstool-6.8.1/
+#export LENSTOOL_ROOT=/users/fgilles/Projects/lenstool-6.8.1/src/
+#export LENSTOOLHPC_ROOT=/users/fgilles/GPU-Projects/lenstool-hpc-master/
+export LENSTOOLHPC_ROOT=$MYPATH
+export CFITSIO_ROOT=/scratch/fgilles/Projects/Libs/cfitsio/
+export WCSTOOL_ROOT=/scratch/fgilles/Projects/Libs/wcstools-3.9.4/libwcs/
+export GSL_ROOT=/scratch/fgilles/Projects/Libs/gsl-2.2/
+#
+export LD_LIBRARY_PATH=$LENSTOOLHPC_ROOT/src:$GSL_ROOT/lib:$LD_LIBRARY_PATH 
+#export PATH=/cm/shared/apps/intel-2017/vtune_amplifier_xe/bin64/:$PATH
+#
+module load intel/compiler intel/mpi intel/mkl
+export CUDA_ROOT=/usr/local/cuda/
+export PATH=$CUDA_ROOT/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_ROOT/lib64:$LD_LIBRARY_PATH
+#
+#module load cuda92
+#
+export CXX=mpiicpc
