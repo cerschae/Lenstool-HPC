b74535da73d7e4805554e65b75f8d9d141f3f688
gilles fourestey <gilles.fourestey@epfl.ch>
2018-11-15 20:29:55 +0100

    new routine to read input files WITHOUT allocating the arrays at the same time
    Former-commit-id: 4a31682c67d241d46490b401998b2ff55ef0a08c

--- src/module_readParameters.hpp
+++ src/module_readParameters.hpp
@@ -41,6 +41,7 @@ void module_readParameters_Potential(std::string infile, Potential lens[], int n
 void module_readParameters_PotentialSOA_2(std::string infile, Potential *lens, Potential_SOA *lens_SOA, int Nset[]);
 void module_readParameters_PotentialSOA(std::string infile, Potential *lens, Potential_SOA *lens_SOA, int nhalos);
 void module_readParameters_PotentialSOA_direct(std::string infile, Potential_SOA *lens_SOA, int nhalos, int n_tot_halos, cosmo_param cosmology);
+void module_readParameters_PotentialSOA_noalloc(std::string infile, Potential_SOA *lens_SOA, int nhalos, int n_tot_halos, cosmo_param cosmology);
 //
 void read_potentialSOA_ntypes(std::string infile, int N_type[]);
 //void module_readParameters_PotentialSOA_nonsorted(std::string infile, Potential *lens, Potential_SOA *lens_SOA, int nhalos);
